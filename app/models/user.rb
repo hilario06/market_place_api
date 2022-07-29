9847c81a15beeaf55fb2c61c0921b2fb9d492bd2
luis <hilario.huaman.6@gmail.com>
2022-07-28 21:15:24 -0500

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password # para el proceso automatico de encriptacion bcrypt
+
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
