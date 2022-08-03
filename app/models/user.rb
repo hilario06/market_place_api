954137be9a8a8e7e7f63b08e789678a1bdc9e22b
luis <hilario.huaman.6@gmail.com>
2022-08-03 11:59:44 -0500

    Generate product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password # para el proceso automatico de encriptacion bcrypt
 
+  has_many :products, dependent: :destroy
+
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
