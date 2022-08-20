2c6fd55cd3cb709c81791b2c84ae7684fd2b1649
luis <hilario.huaman.6@gmail.com>
2022-08-20 18:05:59 -0500

    Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password # para el proceso automatico de encriptacion bcrypt
 
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
